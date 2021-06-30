c378510962622369c5d8df28ef4dbcb915845003
redpeacock78 <scarletpeacock78@gmail.com>
2021-06-30 22:30:58 +0900

    Change content: twigrass.sh

--- twigrass.sh
+++ twigrass.sh
@@ -1,23 +1,23 @@
 #! /usr/bin/env bash
+#shellcheck disable=SC2153
 
-readonly twitter_user="${twitter_user_name}"
-readonly pixela_user="${pixela_user_name}"
-readonly pixela_graph_id="${graph_id}"
-readonly pixela_token="${pixela_user_token}"
+readonly twitter_user="${TWITTER_USER_NAME}"
+readonly pixela_user="${PIXELA_USER_NAME}"
+readonly pixela_graph_id="${PIXELA_GRAPH_ID}"
+readonly pixela_token="${PIXELA_USER_TOKEN}"
 
+declare data
 data="$(curl -s "https://twilog.org/${twitter_user}/stats")"
 
-tweets=($(grep ^'ar_data\[1\]' <<<"${data}"|sed 's/^.*\[1\] = \[//;s/\]\;//;s/,/\n/g' 2>/dev/null|tac|head -n365))
-dates=($(grep ^'ar_lbl\[1\]' <<<"${data}"|sed "s/^.*\[1\] = \[//;s/\]\;//;s/'//g;s/,/\n/g" 2>/dev/null|tac|head -n365))
+mapfile -t tweets < <(grep ^'ar_data\[1\]' <<<"${data}" | sed 's/^.*\[1\] = \[//;s/\]\;//;s/,/\n/g' | tac 2>/dev/null | head -n365)
+mapfile -t dates < <(grep ^'ar_lbl\[1\]' <<<"${data}" | sed "s/^.*\[1\] = \[//;s/\]\;//;s/'//g;s/,/\n/g" | tac 2>/dev/null | head -n365)
 
-for ((i=0; i<"${#tweets[@]}"; i++)); do
-  if [[ "${i}" != "0" ]]; then
-    echo ""
-  fi
-  echo https://pixe.la/v1/users/"${pixela_user}"/graphs/"${pixela_graph_id}"/20${dates[i]} { \"quantity\": \""${tweets[i]}"\"}
+for ((i = 0; i < "${#tweets[@]}"; i++)); do
+  [[ "${i}" != "0" ]] && echo ""
+  echo "https://pixe.la/v1/users/${pixela_user}/graphs/${pixela_graph_id}/20${dates[i]}" '{ "quantity": "'"${tweets[i]}"'"}'
   curl -s -X PUT \
     -H "X-USER-TOKEN:${pixela_token}" \
     -d '{ "quantity": "'"${tweets[i]}"'"}' \
     "https://pixe.la/v1/users/${pixela_user}/graphs/${pixela_graph_id}/20${dates[i]}"
-    sleep 2
+  sleep 2
 done

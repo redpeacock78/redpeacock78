819775a543ac1997a6dc7248ecad50b9840d6c86
redpeacock78 <scarletpeacock78@gmail.com>
2021-06-30 23:07:43 +0900

    Change content: twigrass.sh

--- twigrass.sh
+++ twigrass.sh
@@ -7,17 +7,18 @@ readonly pixela_graph_id="${PIXELA_GRAPH_ID}"
 readonly pixela_token="${PIXELA_USER_TOKEN}"
 
 declare data
-data="$(curl -s "https://twilog.org/${twitter_user}/stats")"
+data="$(curl -s "https://twilog.org/kazuki_199778/stats")"
 
 mapfile -t tweets < <(grep ^'ar_data\[1\]' <<<"${data}" | sed 's/^.*\[1\] = \[//;s/\]\;//;s/,/\n/g' | tac 2>/dev/null | head -n365)
 mapfile -t dates < <(grep ^'ar_lbl\[1\]' <<<"${data}" | sed "s/^.*\[1\] = \[//;s/\]\;//;s/'//g;s/,/\n/g" | tac 2>/dev/null | head -n365)
 
 for ((i = 0; i < "${#tweets[@]}"; i++)); do
   [[ "${i}" != "0" ]] && echo ""
-  echo "https://pixe.la/v1/users/${pixela_user}/graphs/${pixela_graph_id}/20${dates[i]}" '{ "quantity": "'"${tweets[i]}"'"}'
-  curl -s -X PUT \
-    -H "X-USER-TOKEN:${pixela_token}" \
-    -d '{ "quantity": "'"${tweets[i]}"'"}' \
-    "https://pixe.la/v1/users/${pixela_user}/graphs/${pixela_graph_id}/20${dates[i]}"
-  sleep 2
+  declare URL="https://pixe.la/v1/users/${pixela_user}/graphs/${pixela_graph_id}/20${dates[i]}"
+  echo "${URL} { \"quantity\": \"${tweets[i]}\"}" &&
+    curl -s -X PUT \
+      -H "X-USER-TOKEN:${pixela_token}" \
+      -d '{ "quantity": "'"${tweets[i]}"'"}' \
+      "${URL}" &&
+    sleep 2
 done

2ee0762685cf0465af8766ddb0c6a22ad4678c45
redpeacock78 <scarletpeacock78@gmail.com>
2021-07-01 09:50:02 +0900

    Change content: twigrass.sh

--- twigrass.sh
+++ twigrass.sh
@@ -9,17 +9,35 @@ readonly pixela_token="${PIXELA_USER_TOKEN}"
 declare data
 data="$(curl -s "https://twilog.org/${twitter_user}/stats")"
 
-mapfile -t tweets < <(grep ^'ar_data\[1\]' <<<"${data}" | sed 's/^.*\[1\] = \[//;s/\]\;//;s/,/\n/g' | tac 2>/dev/null | head -n365)
-mapfile -t dates < <(grep ^'ar_lbl\[1\]' <<<"${data}" | sed "s/^.*\[1\] = \[//;s/\]\;//;s/'//g;s/,/\n/g" | tac 2>/dev/null | head -n365)
+mapfile -t tweets < <(grep ^'ar_data\[1\]' <<<"${data}" | sed 's/^.*\[1\] = \[//;s/\]\;//;s/,/\n/g;s/\t//g' | tac 2>/dev/null)
+mapfile -t dates < <(grep ^'ar_lbl\[1\]' <<<"${data}" | sed "s/^.*\[1\] = \[//;s/\]\;//;s/'//g;s/,/\n/g;s/\t//g" | tac 2>/dev/null)
 
-for ((i = 0; i < "${#tweets[@]}"; i++)); do
-  if [[ "${i}" != "0" ]]; then
-    echo ""
-  fi
-  echo https://pixe.la/v1/users/"${pixela_user}"/graphs/"${pixela_graph_id}"/20${dates[i]} { \"quantity\": \""${tweets[i]}"\"}
-  curl -s -X PUT \
-    -H "X-USER-TOKEN:${pixela_token}" \
-    -d '{ "quantity": "'"${tweets[i]}"'"}' \
-    "https://pixe.la/v1/users/${pixela_user}/graphs/${pixela_graph_id}/20${dates[i]}" &&
-    sleep 2
-done
+if [[ ! -e ./dates_cache.txt ]]; then
+  for ((i = 0; i < "${#dates[@]}"; i++)); do
+    [[ "${i}" != "0" ]] && echo ""
+    echo https://pixe.la/v1/users/"${pixela_user}"/graphs/"${pixela_graph_id}"/20${dates[i]} { \"quantity\": \""${tweets[i]}"\"} &&
+      curl -s -X PUT \
+        -H "X-USER-TOKEN:${pixela_token}" \
+        -d '{ "quantity": "'"${tweets[i]}"'"}' \
+        "https://pixe.la/v1/users/${pixela_user}/graphs/${pixela_graph_id}/20${dates[i]}" &&
+      sleep 1
+  done
+else
+  mapfile -t dates_cache < <(tr ' ' \\n <./dates_cache.txt)
+  unset "dates_cache[0]"
+  dates_cache=("${dates_cache[@]}")
+  mapfile -t dates_diff < <({
+    echo "${dates_cache[@]}"
+    echo "${dates[@]}"
+  } || tr ' ' \\n | sort | uniq -u | tac)
+  for ((i = 0; i < "${#dates_diff[@]}"; i++)); do
+    echo https://pixe.la/v1/users/"${pixela_user}"/graphs/"${pixela_graph_id}"/20${dates_diff[i]} { \"quantity\": \""${tweets[i]}"\"} &&
+      curl -s -X PUT \
+        -H "X-USER-TOKEN:${pixela_token}" \
+        -d '{ "quantity": "'"${tweets[i]}"'"}' \
+        "https://pixe.la/v1/users/${pixela_user}/graphs/${pixela_graph_id}/20${dates_diff[i]}" &&
+      sleep 1
+  done
+fi
+
+echo "${dates[@]}" >./dates_cache.txt
